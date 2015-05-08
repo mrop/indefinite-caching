589428b23f7d692fb2a846c91f8abb5f910148b1
Michiel Rop <m.rop@onehippo.com>
2015-05-08 14:37:58 +0200

    added new support

---
+++ indefinite-caching/site/src/main/java/org/example/beans/NewsDocument.java
@@ -0,0 +1,124 @@
+package org.example.beans;
+/*
+ * Copyright 2014 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.util.Calendar;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+@HippoEssentialsGenerated(internalName = "indefinitecaching:newsdocument")
+@Node(jcrType="indefinitecaching:newsdocument")
+public class NewsDocument extends HippoDocument {
+
+    /**
+     * The document type of the news document.
+     */
+    public final static String DOCUMENT_TYPE = "indefinitecaching:newsdocument";
+
+    private final static String TITLE = "indefinitecaching:title";
+    private final static String DATE = "indefinitecaching:date";
+    private final static String INTRODUCTION = "indefinitecaching:introduction";
+    private final static String IMAGE = "indefinitecaching:image";
+    private final static String CONTENT = "indefinitecaching:content";
+    private final static String LOCATION = "indefinitecaching:location";
+    private final static String AUTHOR = "indefinitecaching:author";
+    private final static String SOURCE = "indefinitecaching:source";
+
+    /**
+     * Get the title of the document.
+     *
+     * @return the title
+     */
+    @HippoEssentialsGenerated(internalName = "indefinitecaching:title")
+    public String getTitle() {
+        return getProperty(TITLE);
+    }
+
+    /**
+     * Get the date of the document.
+     *
+     * @return the date
+     */
+    @HippoEssentialsGenerated(internalName = "indefinitecaching:date")
+    public Calendar getDate() {
+        return getProperty(DATE);
+    }
+
+    /**
+     * Get the introduction of the document.
+     *
+     * @return the introduction
+     */
+    @HippoEssentialsGenerated(internalName = "indefinitecaching:introduction")
+    public String getIntroduction() {
+        return getProperty(INTRODUCTION);
+    }
+
+    /**
+     * Get the image of the document.
+     *
+     * @return the image
+     */
+    @HippoEssentialsGenerated(internalName = "indefinitecaching:image")
+    public HippoGalleryImageSet getImage() {
+        return getLinkedBean(IMAGE, HippoGalleryImageSet.class);
+    }
+
+    /**
+     * Get the main content of the document.
+     *
+     * @return the content
+     */
+    @HippoEssentialsGenerated(internalName = "indefinitecaching:content")
+    public HippoHtml getContent() {
+        return getHippoHtml(CONTENT);
+    }
+
+    /**
+     * Get the location of the document.
+     *
+     * @return the location
+     */
+    @HippoEssentialsGenerated(internalName = "indefinitecaching:location")
+    public String getLocation() {
+        return getProperty(LOCATION);
+    }
+
+    /**
+     * Get the author of the document.
+     *
+     * @return the author
+     */
+    @HippoEssentialsGenerated(internalName = "indefinitecaching:author")
+    public String getAuthor() {
+        return getProperty(AUTHOR);
+    }
+
+    /**
+     * Get the source of the document.
+     *
+     * @return the source
+     */
+    @HippoEssentialsGenerated(internalName = "indefinitecaching:source")
+    public String getSource() {
+        return getProperty(SOURCE);
+    }
+
+}
+
